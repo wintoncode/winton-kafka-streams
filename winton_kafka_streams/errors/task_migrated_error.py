a7026250c90495e8f0eef9d91da2abfc7983a3ea
Jos Hickson <jos.hickson@gmail.com>
2017-11-28 15:06:43 +0000

    Moved doc string to class itself.

--- winton_kafka_streams/errors/task_migrated_error.py
+++ winton_kafka_streams/errors/task_migrated_error.py
@@ -1,9 +1,9 @@
-"""
-Indicates that a task got migrated to another thread.
-Thus, the task raising this exception can be cleaned up and closed as "zombie".
-"""
 from .kafka_streams_error import KafkaStreamsError
 
 
 class TaskMigratedError(KafkaStreamsError):
+    """
+    Indicates that a task got migrated to another thread.
+    Thus, the task raising this exception can be cleaned up and closed as "zombie".
+    """
     pass

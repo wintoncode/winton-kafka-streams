0bece033958ff3544b328ed41a506654cab81bf6
Andreas Heider <a.heider@winton.com>
2017-12-05 14:47:51 +0000

    Initialise state stores, passing in context

--- winton_kafka_streams/processor/_stream_task.py
+++ winton_kafka_streams/processor/_stream_task.py
@@ -66,7 +66,13 @@ class StreamTask:
         self.commitOffsetNeeded = False
         self.consumedOffsets = {}
 
+        self._init_state_stores()
         self._init_topology(self.context)
+
+    def _init_state_stores(self):
+        self.log.debug(f'Initialising state stores')
+        for store in self.topology.state_stores.values():
+            store.initialise(self.context, store)
 
     def _init_topology(self, context):
         for node in self.topology.nodes.values():
